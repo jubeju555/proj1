cd637600db713bc15576d3399fb5833c0d2a573b
Keaton William Wyrick <kwyrick4@vols.utk.edu>
2025-01-28 12:17:07 -0500

    fixed all errors with printing and passed all gradescripts

--- lib_info.cpp
+++ lib_info.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <fstream>
 #include <map>
+#include <iomanip>
 #include <iterator>
 
 using namespace std;
@@ -13,43 +14,36 @@ struct song
     int tracknum;
     string title;
     string length;
+    song(){
+        tracknum = 0;
+        title = "";
+        length = "";
+    }
 };
 struct album
 {
     string title;
-    int time_length = 0;
-    int num_songs = 0;
-    int num_tracks = 0;
-    map<string, song> tracks;
-
+    int time_length;
+    int num_tracks;
+    map<int, song> tracks;
+    album(){
+        title = "";
+        time_length = 0;
+        num_tracks = 0;
+    }
     int getTotalSeconds();
 };
-// Calculate and return the total number of
-// seconds of all of the tracks in an album
-
-// int album::getTotalSeconds()
-// {
-//     int total_seconds = 0;
-//     string minutes = 0;
-//     string seconds = 0;
-//     map<string, song>::iterator song_it;
-//
-//     for (song_it = tracks.begin(); song_it!=tracks.end(); song_it++)
-//     {
-//         istringstream ss(song_it->first);
-//         getline(ss, minutes, ':');
-//         getline(ss, seconds, ' ');
-//         // error is due to not being c++11, should be fine
-//         total_seconds += (stoi(minutes) * 60) + stoi(seconds);
-//     }
-//     return total_seconds;
-// }
 struct artist
 {
     string name;
-    int num_songs = 0;
-    string total_time = 0;
+    int num_songs;
+    int total_time;
     map<string, album> albums;
+    artist(){
+        name = "";
+        num_songs = 0;
+        total_time = 0;
+    }
     void printfile(string filename);
 };
 
@@ -57,23 +51,30 @@ string removeSpaces(string s)
 {
     string new_string = "";
     stringstream ss(s);
-    while (ss.good())
+    string word = "";
+    while (getline(ss, word, '_'))
     { // used Geeks4Geeks to referesh on stringstreams
-        string word = "";
-        getline(ss, word, '_');
         // if formatting gets weird, add to a vector and parse that way
         new_string = new_string + word + " ";
     }
+    new_string.pop_back();
     return new_string;
+}
+void removeUnderscores(string &s){
+    for(int i=0; i<s.length(); i++){
+        if(s[i]=='_') s[i]=' ';
+    }
 }
 
 int toSeconds(string sec){
     stringstream ss(sec);
-    string time_string;
+    string time_string = "";
     int seconds = 0;
     getline(ss, time_string, ':');
+    // cout<<endl<<time_string<<endl<<endl;
     seconds += stoi(time_string)*60;
     getline(ss, time_string);
+    // cout<<endl<<time_string<<endl<<endl;
     seconds += stoi(time_string);
     return seconds;
 }
@@ -94,17 +95,19 @@ map<string, artist> storefile(string filename)
     map<string, artist> judah;
     while (getline(fileIn, line))
     {
-        removeSpaces(line);
         stringstream ss(line);
         ss >> song_title >> time >> artist_name >> album_name >> genre >> track;
-            
+        removeUnderscores(song_title);
+        removeUnderscores(artist_name);
+        removeUnderscores(album_name);
+        removeUnderscores(genre);
+        
         // store artist in map if not found
         map<string, artist>::iterator artist_it = judah.find(artist_name);
         if (artist_it == judah.end())
         {
             // make artist, fill variables, and insert
-            artist BobMarley;
-            cout<<endl<<"TESTER KEATON"<<endl<<endl;
+            struct artist BobMarley;
             BobMarley.name = artist_name;
             artist_it = judah.insert({artist_name, BobMarley}).first;
         }
@@ -114,18 +117,20 @@ map<string, artist> storefile(string filename)
             //make album, fill variables, insert
             album Exodus;
             Exodus.title = album_name;
-            Exodus.time_length += toSeconds(time);
             album_it=artist_it->second.albums.insert({album_name, Exodus}).first;
         }
 
-        map<string, song>::iterator song_it = album_it->second.tracks.find(song_title);
+        map<int, song>::iterator song_it = album_it->second.tracks.find(track);
         if(song_it == album_it->second.tracks.end()){
             artist_it->second.num_songs++;
+            album_it->second.num_tracks++;
+            album_it->second.time_length+=toSeconds(time);
+            artist_it->second.total_time += toSeconds(time);
             song OneLove;
             OneLove.tracknum = track;
             OneLove.title = song_title;
             OneLove.length = time;
-            album_it->second.tracks.insert({song_title, OneLove});
+            album_it->second.tracks.insert({track, OneLove});
             // store song in album
 
             count++;
@@ -133,54 +138,24 @@ map<string, artist> storefile(string filename)
     }
     return judah;
 }
-
-
-// print file accordngi to the format
-/* 
-void artist::printfile(string filename)
-{
-    // read the file in and store it, (posibly in vector)
-    ifstream fileIn(filename);
-    string line = "";
-    string title = "";
+string secondsToString(int sec){
     string time = "";
-    if (fileIn.is_open())
-    {
-        while (getline(fileIn, line))
-        {
-            stringstream ss(line);
-            ss >> title >> time;
-            cout << name << ", " << albums << endl;
-            for (int i = 0; i < albums.size(); i++)
-            {
-                cout << albums[i].title << albums[i].tracks.size() <<albums[i].getTotalSeconds() << endl;
-                cout << artist_name[i].title << " " << albums[i].tracks.size() << " " << albums[i].getTotalSeconds() << endl;
-                printf("%s %d %d\n", albums[i].title, albums[i].tracks.size(), albums[i].getTotalSeconds());
-                for (int j = 0; j < albums[i].tracks.size(); j++)
-                {
-                    cout << albums[i].tracks[j].title << " " << albums[i].tracks[j].length << endl;
-                }
-
-                // cout << albums[i].tracks[0].title << " " << albums[i].tracks[0].length << endl;
-                // cout << albums[i].tracks[1].title << " " << albums[i].tracks[1].length << endl;
-                
-            }
-            cout << title << " " << time << endl;
-        }
-    }
-
-    return;
+    time += to_string(sec/60);
+    time += ":";
+    if(sec%60<10) time+= "0";
+    time += to_string(sec%60);
+    return time;
 }
-*/
 void printfile(string filename){
     // read the file in and store it, (posibly in vector)
     map<string, artist> judah = storefile(filename);
     for(map<string,artist>::iterator it = judah.begin(); it!=judah.end(); it++){
-        cout<<it->second.name<<": "<<it->second.num_songs<<", "<<it->second.total_time<<endl;
+        cout<<it->second.name<<": "<<it->second.num_songs<<", "<<secondsToString(it->second.total_time)<<endl;
         for(map<string,album>::iterator jt = it->second.albums.begin(); jt!=it->second.albums.end(); jt++){
-            cout<<"        "<<jt->second.title<<": "<<jt->second.num_tracks<<", "<<jt->second.time_length<<endl;
-            for(map<string,song>::iterator nt = jt->second.tracks.begin(); nt!=jt->second.tracks.end(); nt++){
-                cout<<nt->second.tracknum<<". "<<nt->second.title<<": "<<nt->second.length;
+            cout<<setw(8)<<right<<""<<jt->second.title<<": "<<jt->second.num_tracks<<", "<<secondsToString(jt->second.time_length)<<endl;
+            for(map<int,song>::iterator nt = jt->second.tracks.begin(); nt!=jt->second.tracks.end(); nt++){
+                cout<<right<<setw(16)<<""<<nt->second.tracknum<<". "<<nt->second.title<<": "<<nt->second.length;
+                cout<<endl;
             }
         }
     }
